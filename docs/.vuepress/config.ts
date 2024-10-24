8fa18c4f9788a430a6cfc7a06a3d20429c984a59
mahao13 <mahao13@yonyou.com>
2024-10-24 22:38:25 +0800

    fix(git actions报错): 处理页面无法访问vuepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -12,7 +12,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     // 添加您的部署域名
-    hostname: 'https://jiuxi521.github.io/',
+    hostname: 'https://github.com/jiuxi521/',
 
     // 配置侧边栏
     sidebar: {
